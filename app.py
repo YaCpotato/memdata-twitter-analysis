91b2df1c42b28d2e363064d97cb2a0c341d83b19
YaCpotato <yasshisshy@gmail.com>
2020-09-27 20:57:54 +0900

    メインファイル記述

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('./data/tweet_activity_metrics_yassh_i_20200829_20200926_en.csv')
+
+st.title('Members data #4 Twitter Analysis')
+
